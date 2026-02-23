cf336d080166bed28443aa9b01c3cb7cc632dcc7
Tevezx <teveztwitch@gmail.com>
2026-02-23 19:26:59 -0300

    feat: adicionando arrays com possibilidade de abrir e fechar menu com icones interativos

--- web-data-viz/public/js/index.js
+++ web-data-viz/public/js/index.js
@@ -1,4 +1,10 @@
-const menu_header = document.getElementById("menu");
+var menu_header = document.getElementById("menu");
+var imagem_menu = document.getElementById("imagem-menu");
+
+const imagens = [
+    './assets/icon/menu.png',
+    './assets/icon/botao-fechar.png'
+];
 
 function resTablet() {
     if (window.innerWidth >= 768) {
@@ -7,11 +13,13 @@ function resTablet() {
 }
 
 function menu() {
-    if (menu_header.style.display === "block") {
-        menu_header.style.display = "";
-    } else {
-        menu_header.style.display = "block";
-    }
+        if (menu_header.style.display === "block") {
+            menu_header.style.display = "";
+            imagem_menu.src = imagens[0];
+        } else {
+            menu_header.style.display = "block";
+            imagem_menu.src = imagens[1];
+        }
 }
 
 window.addEventListener("resize", resTablet);
