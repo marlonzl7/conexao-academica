2ba7947f175df2b208b1e6213f463ed2a3a2f26a
RayzaDSbr <127635010+RayzaDSbr@users.noreply.github.com>
2026-02-23 20:37:08 -0300

    feat: adicionar validação da input senha

--- web-data-viz/public/javascript/login.js
+++ web-data-viz/public/javascript/login.js
@@ -14,9 +14,9 @@ function mostrarSenha(idInput, icone) {
 }
 
 //validação email 
-
 function onkey_email() {
-    email = ipt_email.value.trim();
+    email = ipt_email.value.trim(); //.trim é para tirar todos os espaços em branco
+    
     let erro = "";
 
     if (email == '') {
@@ -32,3 +32,37 @@ function onkey_email() {
     }
 }
 
+//validação senha
+
+function onkey_senha() {
+    senha = ipt_senha.value.trim();
+    let erro = "";
+
+    if (senha == '') {
+        erro = `Preencha o campo Senha`;
+    } else if (senha.indexOf("0") == -1 && senha.indexOf("1") == -1 && senha.indexOf("2") == -1 &&
+        senha.indexOf("3") == -1 && senha.indexOf("4") == -1 && senha.indexOf("5") == -1 &&
+        senha.indexOf("6") == -1 && senha.indexOf("7") == -1 && senha.indexOf("8") == -1 &&
+        senha.indexOf("9") == -1) {
+        erro = 'A senha deve conter pelo menos um número!';
+    }
+
+    if (erro != "") {
+        div_msg_senha.innerHTML = `${erro}`;
+    } else {
+        div_msg_senha.innerHTML = ``;
+    }
+}
+
+//Validação login 
+
+function entrar() {
+    email = ipt_email.value;
+    senha = ipt_senha.value;
+
+    if (email || senha == '') {
+        div_msg_login.innerHTML = "Por favor, preencha todos os campos."
+    }
+ 
+}
+   
