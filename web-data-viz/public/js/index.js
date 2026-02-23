de3e9b6a04c71577a0e551b46b4b93c1eca58ee1
Tevezx <teveztwitch@gmail.com>
2026-02-23 19:54:47 -0300

    feat: adicionando novas imagens ao header

--- web-data-viz/public/js/index.js
+++ web-data-viz/public/js/index.js
@@ -1,9 +1,15 @@
 var menu_header = document.getElementById("menu");
 var imagem_menu = document.getElementById("imagem-menu");
+var imagem_sobreNos = document.getElementById("sobre-imagem");
+var imagem_contactUs = document.getElementById("contato-imagem");
+var imagem_login = document.getElementById("login-imagem");
 
 const imagens = [
     './assets/icon/menu.png',
-    './assets/icon/botao-fechar.png'
+    './assets/icon/botao-fechar.png',
+    './assets/icon/sobre-nos.png',
+    './assets/icon/contact-us.png',
+    './assets/icon/login.png'
 ];
 
 function resTablet() {
@@ -13,13 +19,16 @@ function resTablet() {
 }
 
 function menu() {
-        if (menu_header.style.display === "block") {
-            menu_header.style.display = "";
-            imagem_menu.src = imagens[0];
-        } else {
-            menu_header.style.display = "block";
-            imagem_menu.src = imagens[1];
-        }
+    if (menu_header.style.display === "block") {
+        menu_header.style.display = "";
+        imagem_menu.src = imagens[0];
+    } else {
+        menu_header.style.display = "block";
+        imagem_menu.src = imagens[1];
+        imagem_sobreNos.src = imagens[2];
+        imagem_contactUs.src = imagens[3];
+        imagem_login.src = imagens[4];
+    }
 }
 
 window.addEventListener("resize", resTablet);
