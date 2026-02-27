82bced2b8b958133da614d41fbc8aa0dae065d36
Tevezx <teveztwitch@gmail.com>
2026-02-27 17:44:15 -0300

    feat: Adicionando funcionalidade no carrossel

--- web-data-viz/public/js/index.js
+++ web-data-viz/public/js/index.js
@@ -1,37 +1,51 @@
 var menu_header = document.getElementById("menu");
-var imagem_menu = document.getElementById("imagem-menu");
-var imagem_sobreNos = document.getElementById("sobre-imagem");
-var imagem_contactUs = document.getElementById("contato-imagem");
-var imagem_login = document.getElementById("login-imagem");
-var imagem_home = document.getElementById("home-imagem")
+
+var carrosel_card_visao = document.getElementById("carrosel-card-visao");
+var carrosel_card_analise = document.getElementById("carrosel-card-analise");
+var carrosel_card_previsao = document.getElementById("carrosel-card-previsao");
+var carrosel_card_gestao = document.getElementById("carrosel-card-gestao");
 
 const imagens = [
-    './assets/icon/menu.png',
-    './assets/icon/botao-fechar.png',
-    './assets/icon/sobre-nos.png',
-    './assets/icon/contact-us.png',
-    './assets/icon/login.png',
-    './assets/icon/home.png'
+  "./assets/icon/menu.png",
+  "./assets/icon/botao-fechar.png",
+  "./assets/icon/sobre-nos.png",
+  "./assets/icon/contact-us.png",
+  "./assets/icon/login.png",
+  "./assets/icon/home.png",
 ];
 
 function resTablet() {
-    if (window.innerWidth >= 768) {
-        menu_header.style.display = "";
-    }
+  if (window.innerWidth >= 768) {
+    menu_header.style.display = "";
+  }
 }
 
 function menu() {
-    if (menu_header.style.display === "block") {
-        menu_header.style.display = "";
-        imagem_menu.src = imagens[0];
-    } else {
-        menu_header.style.display = "block";
-        imagem_menu.src = imagens[1];
-        imagem_sobreNos.src = imagens[2];
-        imagem_contactUs.src = imagens[3];
-        imagem_login.src = imagens[4];
-        imagem_home.src = imagens[5];
-    }
+  var imagem_menu = document.getElementById("imagem-menu");
+  var imagem_sobreNos = document.getElementById("sobre-imagem");
+  var imagem_contactUs = document.getElementById("contato-imagem");
+  var imagem_login = document.getElementById("login-imagem");
+  var imagem_home = document.getElementById("home-imagem");
+
+  if (menu_header.style.display === "block") {
+    menu_header.style.display = "";
+    imagem_menu.src = imagens[0];
+  } else {
+    menu_header.style.display = "block";
+    imagem_menu.src = imagens[1];
+    imagem_sobreNos.src = imagens[2];
+    imagem_contactUs.src = imagens[3];
+    imagem_login.src = imagens[4];
+    imagem_home.src = imagens[5];
+  }
+}
+
+function proximo() {
+    
+}
+
+function anterior() {
+    
 }
 
 window.addEventListener("resize", resTablet);
