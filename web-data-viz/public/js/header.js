abc1957a3122dabaeb9a8c3c16c557f736d5dcb8
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2026-02-28 21:04:17 -0300

    feat: implementar header com menu hamburguer e melhorar responsividade

---
+++ web-data-viz/public/js/header.js
@@ -0,0 +1,38 @@
+var menu_header = document.getElementById("menu");
+
+const imagens = [
+    "../assets/icon/menu.png",
+    "../assets/icon/botao-fechar.png",
+    "../assets/icon/sobre-nos.png",
+    "../assets/icon/contact-us.png",
+    "../assets/icon/login.png",
+    "../assets/icon/home.png",
+];
+
+function resTablet() {
+    if (window.innerWidth >= 768) {
+        menu_header.style.display = "";
+    }
+}
+
+function menu() {
+    var imagem_menu = document.getElementById("imagem-menu");
+    var imagem_sobreNos = document.getElementById("sobre-imagem");
+    var imagem_contactUs = document.getElementById("contato-imagem");
+    var imagem_login = document.getElementById("login-imagem");
+    var imagem_home = document.getElementById("home-imagem");
+
+    if (menu_header.style.display === "block") {
+        menu_header.style.display = "";
+        imagem_menu.src = imagens[0];
+    } else {
+        menu_header.style.display = "block";
+        imagem_menu.src = imagens[1];
+        imagem_sobreNos.src = imagens[2];
+        imagem_contactUs.src = imagens[3];
+        imagem_login.src = imagens[4];
+        imagem_home.src = imagens[5];
+    }
+}
+
+window.addEventListener("resize", resTablet);
