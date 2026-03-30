4764a273cedf403d3b94d79b9155dcfd7db6167a
lucasgslima558 <lucas.slima@sptech.school>
2026-03-30 20:25:19 -0300

    feature - adição do modal de cadastro

---
+++ web-data-viz/public/js/regras.js
@@ -0,0 +1,7 @@
+function abrirModal() {
+    document.getElementById("modal-overlay").classList.remove("hidden");
+}
+
+function fecharModal() {
+    document.getElementById("modal-overlay").classList.add("hidden");
+}
