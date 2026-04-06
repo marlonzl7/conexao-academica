f9180ede833ce3f9d8f4f4251b42d900e125d035
lucasgslima558 <lucas.slima@sptech.school>
2026-04-06 10:13:41 -0300

    feat: adição do modal de edição e deleção

--- web-data-viz/public/js/regras.js
+++ web-data-viz/public/js/regras.js
@@ -1,7 +1,19 @@
-function abrirModal() {
-    document.getElementById("modal-overlay").classList.remove("hidden");
+function abrirModal(id) {
+    document.getElementById(id).classList.remove("hidden");
 }
 
-function fecharModal() {
-    document.getElementById("modal-overlay").classList.add("hidden");
+function fecharModal(id) {
+    document.getElementById(id).classList.add("hidden");
+}
+
+function cadastrarRegra() {
+
+}
+
+function editarRegra() {
+    
+}
+
+function apagarRegra() {
+
 }
