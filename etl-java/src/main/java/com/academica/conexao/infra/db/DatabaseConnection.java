4665e95289cbc73921b4bb0ccd05907070e1bdf1
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2026-04-23 05:11:18 -0300

    fix: corrigir nome das variáveis de ambiente

--- etl-java/src/main/java/com/academica/conexao/infra/db/DatabaseConnection.java
+++ etl-java/src/main/java/com/academica/conexao/infra/db/DatabaseConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private final String URL = System.getenv("DB_URL");
-    private final String USER = System.getenv("DB_USER");
-    private final String PASSWORD = System.getenv("DB_PASSWORD");
+    private final String URL = System.getenv("ETL_DB_URL");
+    private final String USER = System.getenv("ETL_DB_USER");
+    private final String PASSWORD = System.getenv("ETL_DB_PASSWORD");
 
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
