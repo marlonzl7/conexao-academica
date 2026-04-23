29aaedd4f1c477d02a5ff093a556b5547e65124f
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2026-04-23 05:01:49 -0300

    feat: adicionar script para criação de usuário do banco para as aplicações

--- web-data-viz/src/database/etl-java-db-user.sql
+++
@@ -1,9 +0,0 @@
-USE conexaoacademica;
-
-CREATE USER 'example'@'localhost' IDENTIFIED BY 'example';
-
-GRANT INSERT ON conexaoacademica.instituicao TO 'example'@'localhost';
-GRANT INSERT ON conexaoacademica.curso TO 'example'@'localhost';
-GRANT INSERT ON conexaoacademica.indicadores_curso TO 'example'@'localhost';
-
-FLUSH PRIVILEGES;
--- web-data-viz/src/database/web-app-db-user.sql
+++
@@ -1,7 +0,0 @@
-USE conexaoacademica;
-
-CREATE USER 'example'@'localhost' IDENTIFIED BY 'example';
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON conexaoacademica.* TO 'example'@'localhost';
-
-FLUSH PRIVILEGES;
