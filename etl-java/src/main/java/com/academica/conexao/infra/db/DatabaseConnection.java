e9fda1f1659c61feb3b7e9c108864ab5cdfc2037
98499596+marlonzl7@users.noreply.github.com <sys-admin@ip-172-31-77-18.ec2.internal>
2026-04-25 05:23:48 +0000

    fix: adicionar carregamento explicito do driver MySQL

--- etl-java/src/main/java/com/academica/conexao/infra/db/DatabaseConnection.java
+++ etl-java/src/main/java/com/academica/conexao/infra/db/DatabaseConnection.java
@@ -6,6 +6,14 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("MySQL Driver não encontrado", e);
+        }
+    }
+
     private final String URL = System.getenv("ETL_DB_URL");
     private final String USER = System.getenv("ETL_DB_USER");
     private final String PASSWORD = System.getenv("ETL_DB_PASSWORD");
