eb16fee3ea2c731f0157eb6d75d19fa67a4c5518
lucasgslima558 <lucas.slima@sptech.school>
2026-05-05 11:31:54 -0300

    fix: adição dos campos 'modalidade' em curso e 'cor_hexadecimal' em regra

--- web-data-viz/src/database/01-script.sql
+++ web-data-viz/src/database/01-script.sql
@@ -16,7 +16,8 @@ CREATE TABLE instituicao (
 CREATE TABLE curso (
     id_curso INT PRIMARY KEY AUTO_INCREMENT,
     id_instituicao INT NOT NULL,
-    nome VARCHAR(150) NOT NULL,
+    nome VARCHAR(200) NOT NULL,
+    modalidade VARCHAR(20) NOT NULL,
     CONSTRAINT fk_curso_instituicao FOREIGN KEY (id_instituicao) REFERENCES instituicao(id_instituicao)
 );
 
@@ -48,11 +49,11 @@ CREATE TABLE regra (
     id_instituicao INT NOT NULL,
     id_kpi INT NOT NULL,
     classificacao VARCHAR(20) NOT NULL,
+    cor_hexadecimal CHAR(6),
     limite_inferior DECIMAL(5,2) NOT NULL,
     limite_superior DECIMAL(5,2) NOT NULL,
-    
-    FOREIGN KEY (id_instituicao) REFERENCES instituicao(id_instituicao),
-    FOREIGN KEY (id_kpi) REFERENCES kpi(id_kpi)
+    CONSTRAINT fk_regra_instituicao FOREIGN KEY (id_instituicao) REFERENCES instituicao(id_instituicao),
+    CONSTRAINT fk_regra_kpi FOREIGN KEY (id_kpi) REFERENCES kpi(id_kpi)
 );
 
 CREATE TABLE indicadores_curso (
@@ -62,7 +63,7 @@ CREATE TABLE indicadores_curso (
     quantidade_alunos_situacao_desvinculada INT NOT NULL,
     quantidade_alunos_situacao_trancada INT NOT NULL,
     PRIMARY KEY (id_curso, ano),
-    FOREIGN KEY (id_curso) REFERENCES curso(id_curso)
+    CONSTRAINT fk_indicadorescurso_curso FOREIGN KEY (id_curso) REFERENCES curso(id_curso)
 );
 
 CREATE TABLE mensagem (
