df6d4fa8c6f924aee53d2193c8051adfdde4cdab
Marlon de Souza <98499596+marlonzl7@users.noreply.github.com>
2026-05-10 20:43:42 -0300

    Revert "fix: corrigir formatação e listagem de bases no email do ETL"

--- etl-java/src/main/java/com/academica/conexao/infra/email/service/NotificacaoETLService.java
+++ etl-java/src/main/java/com/academica/conexao/infra/email/service/NotificacaoETLService.java
@@ -51,22 +51,19 @@ public class NotificacaoETLService {
     private String montarMensagemAdmin(ResultadoETL etlResultado) {
         String bases = "Bases processadas:\n";
 
-        for (String base : etlResultado.getBasesProcessadas()) {
-            bases += "   - " + base + "\n";
+        for (int i = 0; i < etlResultado.getBasesProcessadas().size() - 1; i++) {
+            bases += "   - " +  etlResultado.getBasesProcessadas().get(i) + "\n";
         }
 
         return """
             O processo de ETL foi finalizado.
-        
+
             Duração: %.2fs
             Total de linhas processadas: %d
-        
+
             Bases processadas:
             %s
-            """.formatted(
-                        etlResultado.getDuracaoSegundos(),
-                        bases
-        );
+            """.formatted(etlResultado.getDuracaoSegundos(), bases);
     }
 
     private String montarMensagemDiretor(ResultadoETL resultadoETL) {
